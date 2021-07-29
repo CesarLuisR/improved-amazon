52da6b4925ba2278a94a8d96b73361f9c6fd72a6
Cesar Luis Rijo <cesarluisrc@gmail.com>
2021-07-29 16:29:07 -0400

    Service Worker and manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import StoreProvider from "../app/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
+    <>
+      <Head>
+        <title>Amazon</title>
+        <link
+          rel="shortcut icon"
+          href="../public/favicon.jpg"
+          type="image/x-icon"
+        />
+      </Head>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </>
   );
 }
 export default MyApp;
